1c3af38cb9f66542ea04c5fea12f9a38bdea1744
giovannicorte <cortegiovanni98@gmail.com>
2021-05-25 11:44:19 +0200

    Initial Commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Giovanni Corte on 25/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
